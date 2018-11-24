955a8001e7b04169ae56c262df066528be4b8c5f
Clelia Meneghin <cmeneghi@adobe.com>
2018-11-24 16:02:26 +0100

    test for menu

---
+++ documentation/bon-app-menu.js
@@ -0,0 +1,14 @@
+class Menu (){
+	var menu = '<div id="bon-app-menu">\
+	<input id="search-box">\
+	<div id="bon-app-page-list">\
+		<ul>\
+		  <li><a href="#home">Home</a></li>\
+		  <li><a href="#recipes">Recipes</a></li>\
+		  <li><a href="#ingredient">Ingredients</a></li>\
+		  <li><a href="#about">About</a></li>\
+		</ul>\
+	</div>\
+</div>"
+	$("body").append(menu);
+}
