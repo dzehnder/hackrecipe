dcf85d7ca9fbbb92ddb1288695a42da080c25926
Jonas Haberkorn <jonas.haberkorn16@gmail.com>
2018-11-24 16:07:37 +0100

    adding DB_Service

---
+++ DB_Service/baselhack/src/main/java/baselhack/Ingredient.java
@@ -0,0 +1,28 @@
+package baselhack;
+
+public class Ingredient {
+	private String text;
+	private String objectID;
+	
+	public Ingredient() {}
+	
+	public String getText() {
+	  return text;
+	}
+	
+	public Ingredient setText(String text) {
+	  this.text = text;
+	  return this;
+	}
+	
+	public String getObjectID() {
+	  return objectID;
+	}
+	
+	public Ingredient setObjectID(String objectID) {
+	  this.objectID = objectID;
+	  return this;
+	}
+
+}
+
