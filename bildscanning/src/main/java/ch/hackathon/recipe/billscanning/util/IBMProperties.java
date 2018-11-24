70f6dbaf587ae0c6b084625258ceec03f09836cf
Joel Zimmerli <joel.zimmerli@prodyna.com>
2018-11-24 14:06:42 +0100

    Adds propertie Reader for API Key

---
+++ bildscanning/src/main/java/ch/hackathon/recipe/billscanning/util/IBMProperties.java
@@ -0,0 +1,82 @@
+package com.prodyna.NewsHandler.utilities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class IBMProperties {
+    public static void main(String[] args) {
+        IBMProperties app = new IBMProperties();
+        app.printThemAll();
+        System.out.println(app.getPropertie("apikey"));
+    }
+
+    private void printThemAll() {
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            String filename = "IBM_Cloud.properties";
+            input = getClass().getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + filename);
+                return;
+            }
+
+            prop.load(input);
+
+            Enumeration<?> e = prop.propertyNames();
+            while (e.hasMoreElements()) {
+                String key = (String) e.nextElement();
+                String value = prop.getProperty(key);
+                System.out.println("Key : " + key + ", Value : " + value);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public String getPropertie(String key) {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            String filename = "IBM_Cloud.properties";
+            input = getClass().getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + filename);
+                throw new IllegalArgumentException("Sorry, unable to find " + filename);
+            }
+
+            prop.load(input);
+            return prop.getProperty(key);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new IllegalArgumentException("Property File Not Found");
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
